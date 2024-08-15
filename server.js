238dd94868754f08611c34d0add741a7c4efd1aa
Shameer Chagani <73441777+shameerchagani@users.noreply.github.com>
2024-08-15 12:44:43 +0300

    updated package.json
    Added connect-mongo package.

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const dotenv = require('dotenv').config()
 const express = require('express');
-const multer  = require('multer')
 const path = require('path')
 const router = express.Router();
 const app = express();
 const mongoose = require('mongoose');
 const expressEjsLayout = require('express-ejs-layouts');
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 8083;
 const db = process.env.dbPassword;
 const passport = require('passport');
 require("./config/passport")(passport);
 const flash = require('connect-flash');
 const session = require('express-session');
 const cors = require("cors")
-const helmet = require("helmet")
+const MongoStore = require("connect-mongo"); 
 
 //mongoose connection
 mongoose.connect(db,{useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false})
@@ -29,16 +28,15 @@ app.set('view engine','ejs');
 app.use(express.urlencoded({extended : false}));
 app.use(express.static(__dirname + '/public'));
 
-
-//Initialize Helmet
-//app.use(cors());
-//app.use(helmet({}));
-
 //express session
 app.use(session({
     secret : 'secret',
     resave : true,
-    saveUninitialized : true
+    saveUninitialized : true,
+    store: MongoStore.create({
+      mongoUrl: process.env.dbPassword,
+    })
+	
 }));
 
 app.use(passport.initialize());
